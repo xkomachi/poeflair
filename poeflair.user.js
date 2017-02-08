98b20184111623071e0bd4a1f740fea2630402a7
xkomachi <avi9520@gmail.com>
2017-02-09 01:08:43 +0200

    Update poeflair.user.js

--- poeflair.user.js
+++ poeflair.user.js
@@ -1,1 +1,15 @@
-test
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkeys.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    alert("hihi v2 v3");
+})();
